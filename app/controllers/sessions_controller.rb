7f89f1990e5aef15576f4759abb6ccb97d3370ca
Myo Aung <myo.aung.mm.1995@gmail.com>
2023-04-20 14:18:22 +0900

    Chapter 8 Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.find_by(email: params[:session] [:email].downcase)
+    if user&.authenticate(params[:session][:password]) # same as user && user.authenticate
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      #create error message
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new', status: :unprocessable_entity
+    end
+  end
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end
+end
