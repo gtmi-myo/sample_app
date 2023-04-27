ad19e6e3144a221b275af930b4556de3d83f69bb
Myo Aung <myo.aung.mm.1995@gmail.com>
2023-04-27 16:04:37 +0900

    Chapter 13 13.2 Showing Microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def show
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])  
+    @microposts = @user.microposts.paginate(page: params[:page])  
     
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,4 +76,12 @@ end
   
   end
 
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
 end
